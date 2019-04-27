199b56857a6f529be9e7dfe36356be985d0c53fd
Tadas Kundelis <tadask@wix.com>
2019-04-28 00:32:31 +0300

    added error handling

--- mysqlQueries.js
+++ mysqlQueries.js
@@ -3,18 +3,25 @@ const createPool = require('./connectMysql');
 const pool = createPool();
 
 module.exports = {
-    postComponent: function(compId, instanceId) {
+    postComponent: function(res, compId, instanceId) {
         const query = `SELECT * FROM components WHERE compID = ?`;
         pool.query(query, compId, function(err, rows){
+            if(err) throw err;
             if(!rows.length) {
                 const query = `INSERT INTO components (compID, instanceID, message) VALUES (?, ?, ?)`;
-                pool.query(query, [compId, instanceId, '']);
+                const callback = function(err) {
+                    if(err) throw err;
+                    res.write("Component successfully saved");
+                    res.end();
+                };
+                pool.query(query, [compId, instanceId, ''], callback);
             }
         })
     },
     getMessage: function(res, params) {
         const query = `SELECT message FROM components WHERE compID = ? AND instanceID = ?`;
         const callback = function(err, rows) {
+            if(err) throw err;
             if(rows.length) {
                 res.write(rows[0].message);
                 res.end();
@@ -24,7 +31,8 @@ module.exports = {
     },
     postMessage: function(req, res, params) {
         const query = `UPDATE components SET message = ? WHERE compID = ? AND instanceID = ?`;
-        const callback = function() {
+        const callback = function(err) {
+            if(err) throw err;
             res.write('updated successfully');
             res.end();
         }
